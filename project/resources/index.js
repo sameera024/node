30eda69950b018a602c0fd30e66ea439330de2bb
sameera024 <36402393+sameera024@users.noreply.github.com>
2018-07-07 23:08:44 +0530

    Update index.js

--- project/resources/index.js
+++ project/resources/index.js
@@ -3,5 +3,7 @@ var { ProjectTypeController } = require("./controllers/controller");
 
 module.exports = function () {
     router.use('/projtype', new ProjectTypeController().route());
+   console.log("changed");
+    
     return router;
 }
