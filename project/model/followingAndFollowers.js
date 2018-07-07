98d35a26bc7d085848a2f05d690b23779ad17f41
sameera024 <bsameera024@gmail.com>
2018-07-07 21:52:52 +0530

    space added

--- project/model/followingAndFollowers.js
+++ project/model/followingAndFollowers.js
@@ -3,5 +3,6 @@ var followingAndFollowersSchema = new mongoose.Schema({
     user_id: String,
     followers: [],
     following: []
+
 })
 module.exports = mongoose.model('FollowingAndFollowers', FollowingAndFollowersSchema);
